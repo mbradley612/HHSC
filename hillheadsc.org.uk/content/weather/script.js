dc1a3a868c2a74637404514b83406e969f8b3d3a
Matthew Bradley <mbradley612@gmail.com>
2018-12-30 21:18:20 +0000

    Consolidated JSON in day.json and updated HTML and script to ready from the single file.

--- hillheadsc.org.uk/content/weather/script.js
+++ hillheadsc.org.uk/content/weather/script.js
@@ -1,74 +1,97 @@
 // Load the Visualization API and the piechart package.
 google.charts.load('current', {'packages':['corechart']});
 
-
 //get json data
+
 xhttp = new XMLHttpRequest();
-url = '/weewx/day-wind.json';
+
+dayUrl = '/weewx/day.json';
 
 xhttp.onreadystatechange = function(){
-    if (this.readyState == 4 && this.status == 200) {
-        // Set a callback to run when the Google Visualization API is loaded.
-	r=this.responseText;
-	//console.log(r);
+  if (this.readyState == 4 && this.status == 200) { // Set a callback to run when the Google Visualization API is loaded.
+  r=this.responseText;
+  //console.log(r);
 
-	jsonData = JSON.parse(r).dayWindData;
-	console.log(jsonData);
-	google.charts.setOnLoadCallback(drawChart);        
-    }
-}
+	windJsonData = JSON.parse(r).dayWindData;
+  pressureJsonData =JSON.parse(r).dayPressureData;
+  outTemp = JSON.parse(r).dayOutTemp;
+  inTemp =	JSON.parse(r).dayInTemp;
+  google.charts.setOnLoadCallback(drawCharts); } }
 
-xhttp.open("GET",url);
-xhttp.send();
+xhttp.open("GET",dayUrl); xhttp.send();
 
-function drawChart() {
-    //Create table data
+function drawCharts() { //Create table data
 
     windData = new google.visualization.DataTable();
     windData.addColumn("datetime","Time");
-    windData.addColumn("number","Average Speed");
-    windData.addColumn("number","Gust Speed");
+    windData.addColumn("number","Average Speed (knots)");
+    windData.addColumn("number","Gust Speed (knots)");
 
     dirData = new google.visualization.DataTable();
     dirData.addColumn("datetime","Time");
     dirData.addColumn("number","Direction");
-    
-    
-    jsonData.forEach(function(x){
-	date = new Date(x[0]);
-	windData.addRow([date,x[1],x[2]]);
-	dirData.addRow([date,x[3]]);
+
+    pressureData = new google.visualization.DataTable();
+    pressureData.addColumn("datetime","Time");
+    pressureData.addColumn("number","Pressure (mbars)");
+
+    windJsonData.forEach(function(x){
+      date = new Date(x[0]);
+      windData.addRow([date,x[1],x[2]]); dirData.addRow([date,x[3]]);
     })
-    var windOptions = {
-        title: 'Wind Speed',
-        legend: { position: 'bottom' },
-	hAxis:{
-	    format:"hh:mm"
-	}
-    };
+
+    pressureJsonData.forEach(function(x){
+      date = new Date(x[0]);
+      pressureData.addRow([date, x[1]]);
+    })
+
+    var windOptions = { title: 'Wind Speed',
+    curveType: 'function',
+    legend: {
+    position: 'bottom' },
+    hAxis:{ format:"HH:mm" } };
 
     dirOptions = {
-	title: 'Wind Direction',
-	legend: {position: 'bottom'},
-	hAxis:{
-	    format:"hh:mm"
-	},
-	vAxis:{
-	    viewWindow:{
-		min:0,
-		max:360
-	    }
-	}
+       title: 'Wind Direction',
+       curveType: 'function',
+       legend: {
+         position: 'bottom'
+       },
+       hAxis:{
+         format:"HH:mm"
+       },
+       vAxis:{
+         viewWindow:{
+           min:0,
+           max:360
+         }
+       }
+     };
+
+    pressureOptions = {
+      title: 'Pressure',
+      curveType: 'function',
+      legend: {
+        position: 'bottom'
+      },
+      hAxis:{
+        format:"HH:mm"
+      },
     };
 
-    
+    var windChart = new
+      google.visualization.LineChart(document.getElementById('wind_speed_chart'));
 
-    var windChart = new google.visualization.LineChart(document.getElementById('wind_chart'));
+    var dirChart = new
+      google.visualization.LineChart(document.getElementById('wind_direction_chart'));
 
-    var dirChart = new google.visualization.LineChart(document.getElementById('dir_chart'));
+    var pressureChart = new
+      google.visualization.LineChart(document.getElementById('pressure_chart'));
 
-    
     windChart.draw(windData, windOptions);
 
     dirChart.draw(dirData, dirOptions);
-}
+
+    pressureChart.draw(pressureData, pressureOptions);
+
+  }
